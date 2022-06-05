9dba60cfe5d0b23239c94061fd24b12b1b23f6ce
Wojciech M <wojtekm@myprograme.com>
2022-06-05 09:17:02 +0200

    add file docker

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
+from flask import render_template
+from app.__init__ import app
 
 @app.route('/')
 def hello_world():  # put application's code here
