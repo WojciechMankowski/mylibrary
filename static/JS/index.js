e6c3a67743d212178ffb6b8199dd46ef5a1903e0
Wojciech M <wojtekm@myprograme.com>
2022-06-10 12:21:02 +0200

    change of classes in JS active item to inactive item

--- static/JS/index.js
+++ static/JS/index.js
@@ -1,1 +1,24 @@
+const home = document.getElementById("index");
+const movie = document.getElementById("movie");
+const book = document.getElementById("book");
 
+const IsActive = () => {
+  let name = window.location.pathname;
+  if (name == "/") {
+    home.className = "nav-link active";
+    book.className = "nav-link";
+    movie.className = "nav-link";
+  } else if (name == "/movie") {
+    home.className = "nav-link ";
+    book.className = "nav-link ";
+    movie.className = "nav-link active";
+  } else if (name == "/book") {
+    home.className = "nav-link ";
+    book.className = "nav-link active";
+    movie.className = "nav-link ";
+  }
+};
+
+movie.addEventListener("click", IsActive());
+home.addEventListener("click", IsActive());
+book.addEventListener("click", IsActive());
